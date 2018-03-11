6ff94d083524bee78e17cd49453b00fdc591f5e4
xtvjxk <xtvjxk123456maya@163.com>
2018-03-11 16:29:08 +0800

    [add]创建模块

---
+++ util.py
@@ -0,0 +1,17 @@
+# coding:utf-8
+# 这个模块是在blender 2.8 py3里用的
+import bpy
+
+C = bpy.context
+D = bpy.data
+O = bpy.ops
+
+
+# set object to be active for operator
+def set_object_active(objectName):
+    try:
+        obj = D.objects[objectName]
+    except (KeyError, TypeError):
+        raise Exception("Input argv is error")
+    else:
+        obj.select_set(action='SELECT')

94a1e08db6e6e116531533c387ba1532867a1401
zhangxuqiang <zhangxuqiang@kkanimation.com>
2018-03-12 09:48:59 +0800

    添加一个函数

--- util.py
+++ util.py
@@ -15,6 +15,19 @@ def set_object_active(objectName):
         raise Exception("Input argv is error")
     else:
         obj.select_set(action='SELECT')
+        obj.select_set(action='DESELECT')
         # active应该一直都只有一个
         return C.active_object
+
+
+def select_object(objectName, append=False):
+    if not append:
+        O.object.select_all(action='DESELECT')
+    try:
+        obj = D.objects[objectName]
+    except (KeyError, TypeError):
+        raise Exception("Input argv is error")
+    else:
+        obj.select_set(action='SELECT')
+        return C.selected_objects
 

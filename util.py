7e984a7497c4fccd3afc1545d95f3a2fb74addde
zhangxuqiang <zhangxuqiang@kkanimation.com>
2018-03-12 18:19:50 +0800

    修复一些东西

--- util.py
+++ util.py
@@ -1,9 +1,20 @@
 # coding:utf-8
 import bpy
+import bmesh as bm
 
 C = bpy.context
 D = bpy.data
 O = bpy.ops
+
+
+def is_future_version():
+    version = bpy.app.version
+    if version[0] > 2:
+        return True
+    if version[0] == 2 and version[1] >= 80:
+        return True
+    else:
+        return False
 
 
 # set object to be active for operator
@@ -13,10 +24,14 @@ def set_object_active(objectName):
     except (KeyError, TypeError):
         raise Exception("Input argv is error")
     else:
-        obj.select_set(action='SELECT')
-        obj.select_set(action='DESELECT')
-        # active应该一直都只有一个
-        return C.active_object
+        if is_future_version():
+            obj.select_set(action='SELECT')
+            obj.select_set(action='DESELECT')
+            # active应该一直都只有一个
+            return C.active_object
+        else:
+            C.scene.objects.active = obj
+            return C.active_object
 
 
 # select object
@@ -28,5 +43,31 @@ def select_object(objectName, append=False):
     except (KeyError, TypeError):
         raise Exception("Input argv is error")
     else:
-        obj.select_set(action='SELECT')
-        return C.selected_objects
+        if is_future_version():
+            obj.select_set(action='SELECT')
+            return C.selected_objects
+        else:
+            # 2.80以前有层概念，注意当前层
+            obj.select = True
+            return C.selected_objects
+
+
+def vertexs_position(meshObjectName, is_global=True):
+    obj = set_object_active(meshObjectName)
+    mesh_obj = obj.data
+
+    # #set mesh_obj edit
+    # O.object.mode_set(mode='EDIT')
+    # O.mesh.select_all(action='DESELECT')
+    mesh_bm = bm.new()
+    mesh_bm.from_mesh(mesh_obj)
+
+    verts = mesh_bm.verts
+    if not is_future_version():
+        verts.ensure_lookup_table()
+    if is_global:
+        for v in verts:
+            yield (obj.matrix_world * v.co).to_tuple()
+    else:
+        for v in verts:
+            yield v.co.to_tuple()
